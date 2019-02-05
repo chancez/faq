18e4d0cbaa1a19e07dd61d15d0d9cbd7d68cbc8f
Chance Zibolski <czibolsk@redhat.com>
2019-02-05 14:56:28 -0800

    Allow commiting vendor and don't run go mod vendor in validate

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -4,7 +4,7 @@ set -e
 
 export GO111MODULE=on
 
-go mod vendor
+# go mod vendor
 go vet $(go list ./...)
 diff <(goimports -d $(find . -type f -name '*.go' -not -path "./vendor/*")) <(printf "")
 (for d in $(go list ./...); do diff <(golint $d) <(printf "") || exit 1;  done)
